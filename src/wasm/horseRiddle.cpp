48010c8b7a0bfb8376b697184ac85ed00f44060c
David Kisanga <davidlivingstonekisanga@gmail.com>
2025-07-20 17:21:19 -0400

    fix for wasm bug

--- src/wasm/horseRiddle.cpp
+++ src/wasm/horseRiddle.cpp
@@ -11,21 +11,34 @@ class Horse {
     public:
         uint32_t absolutePosition {};
         bool isDeleted {false};
-        std::vector<uint32_t> slowerThan {};
+        
 
         Horse() : absolutePosition {positionGenerator++} {}
 
+        std::vector<uint32_t> getSlowerThan() {
+            return slowerThan;
+        }
+        void addSlowerThan(uint32_t fasterHorse) {
+            slowerThan.push_back(fasterHorse);
+        }
+
     private:
+        std::vector<uint32_t> slowerThan {};
         static inline uint32_t positionGenerator {0};
 };
 // static inline = initial numbers dont get populated for some reason
 class HorseRiddle {
     public:
-        Horse horses[25];
+        Horse* horses[25] {};
 
-        HorseRiddle() = default;
+        HorseRiddle() {
+            initHorses(12,1000);
+        }
 
-        void randomizeHorseOrder(uint32_t state, uint32_t num_shuffles) {
+        void initHorses(uint32_t state, uint32_t num_shuffles) {
+            for (uint32_t i {0}; i < NUM_HORSES; i++) {
+                horses[i] = new Horse(); // might need new horse here
+            }
             uint32_t idx1 {};
             uint32_t idx2 {};
             for(size_t i {0}; i < num_shuffles; i++) {
@@ -36,9 +49,9 @@ class HorseRiddle {
                 if (idx1 == idx2) {
                     continue;
                 }
-                horses[idx1].absolutePosition = horses[idx1].absolutePosition ^ horses[idx2].absolutePosition;
-                horses[idx2].absolutePosition = horses[idx1].absolutePosition ^ horses[idx2].absolutePosition;
-                horses[idx1].absolutePosition = horses[idx1].absolutePosition ^ horses[idx2].absolutePosition;
+                horses[idx1]->absolutePosition = horses[idx1]->absolutePosition ^ horses[idx2]->absolutePosition;
+                horses[idx2]->absolutePosition = horses[idx1]->absolutePosition ^ horses[idx2]->absolutePosition;
+                horses[idx1]->absolutePosition = horses[idx1]->absolutePosition ^ horses[idx2]->absolutePosition;
             }
         }
     
@@ -62,7 +75,7 @@ class HorseRiddle {
                 if (sources[0] != inputPosition) {
                     return std::vector<uint32_t> {DEFINITE_FASTER_HORSE_CODE, sources[0], inputPosition};
                 }
-                horses[sources[0]].isDeleted = true;
+                horses[sources[0]]->isDeleted = true;
             }
             return std::vector<uint32_t> {HORSE_ORDER_CORRECT_CODE, 0, 0};
         }
@@ -78,8 +91,8 @@ class HorseRiddle {
             uint32_t horseToInsert {};
             for(size_t i {0}; i < inputHorses.size(); i++) {
                 for(uint32_t curHorseIdx : inputHorses) {
-                    uint32_t curHorseAbsolutePosition = horses[curHorseIdx].absolutePosition;
-                    if (curHorseAbsolutePosition < min && (orderedHorses.size() == 0 || curHorseAbsolutePosition > horses[orderedHorses.back()].absolutePosition)) {
+                    uint32_t curHorseAbsolutePosition = horses[curHorseIdx]->absolutePosition;
+                    if (curHorseAbsolutePosition < min && (orderedHorses.size() == 0 || curHorseAbsolutePosition > horses[orderedHorses.back()]->absolutePosition)) {
                         min = curHorseAbsolutePosition;
                         horseToInsert = curHorseIdx;
                     }
@@ -93,7 +106,7 @@ class HorseRiddle {
         void updateOrderGraph(std::vector<uint32_t> orderedHorses) {
             uint32_t fasterHorseIdx = orderedHorses[0]; 
             for (uint32_t i {1}; i < orderedHorses.size(); i++) {
-                horses[orderedHorses[i]].slowerThan.push_back(fasterHorseIdx);
+                horses[orderedHorses[i]]->addSlowerThan(fasterHorseIdx);
                 fasterHorseIdx = orderedHorses[i];
             }
         }
@@ -101,12 +114,12 @@ class HorseRiddle {
         std::vector<uint32_t> getSources() {
             std::vector<uint32_t> sources {};
             for(uint32_t i {0}; i < NUM_HORSES; i++) {
-                if(horses[i].isDeleted) {
+                if(horses[i]->isDeleted) {
                     continue;
                 }
                 uint32_t trueNumSlowerThan {0};
-                for(uint32_t idx : horses[i].slowerThan) {
-                    if (!horses[idx].isDeleted) {
+                for(uint32_t idx : horses[i]->getSlowerThan()) {
+                    if (!horses[idx]->isDeleted) {
                         ++trueNumSlowerThan;
                     }
                 }
@@ -118,15 +131,20 @@ class HorseRiddle {
         }
 };
 
-
-
-// [[clang::export_name("initPuzzle")]]
-void initPuzzle(HorseRiddle& horseRiddle) {
-    horseRiddle.randomizeHorseOrder(12, 1000);
+HorseRiddle& getHorseRiddle()
+{
+    static HorseRiddle horseRiddle;
+    return horseRiddle;
 }
 
-// [[clang::export_name("submitRace")]]
-uint32_t submitRace(uint32_t input, HorseRiddle& horseRiddle) {
+// [[clang::export_name("initPuzzle")]]
+// void initPuzzle(HorseRiddle& horseRiddle) {
+//     horseRiddle.randomizeHorseOrder(12, 1000);
+// }
+
+[[clang::export_name("submitRace")]]
+uint32_t submitRace(uint32_t input) {
+    HorseRiddle horseRiddle = getHorseRiddle();
     // unpack the input and then submit the race
     std::vector<uint32_t> horsesToRace {};
     uint32_t fiveBitMask = 0x1f; // 5 bit mask
@@ -138,8 +156,9 @@ uint32_t submitRace(uint32_t input, HorseRiddle& horseRiddle) {
     return input;
 }
 
-// [[clang::export_name("checkAnswer")]]
-uint32_t checkAnswer(uint32_t input, HorseRiddle& horseRiddle) {
+[[clang::export_name("checkAnswer")]]
+uint32_t checkAnswer(uint32_t input) {
+    HorseRiddle horseRiddle = getHorseRiddle();
     std::vector<uint32_t> submittedHorses {};
     uint32_t fiveBitMask = 0x1f; // 5 bit mask
     for (uint32_t i {0}; i < 3; i++) {
@@ -155,24 +174,23 @@ uint32_t checkAnswer(uint32_t input, HorseRiddle& horseRiddle) {
     return answerIntForm;
 }    
 
-[[clang::export_name("doEverything")]]
-uint32_t doEverything() {
-    HorseRiddle horseRiddle;
-    initPuzzle(horseRiddle);
-    submitRace(34916, horseRiddle);
-    submitRace(5446921, horseRiddle);
-    submitRace(10858926, horseRiddle);
-    submitRace(16270931, horseRiddle);
-    submitRace(21682936, horseRiddle);
-    submitRace(209462, horseRiddle);
-    submitRace(18038548, horseRiddle);
-    return checkAnswer(21270, horseRiddle);
-}
+// [[clang::export_name("doEverything")]]
+// uint32_t doEverything() {
+    
+//     submitRace(34916);
+//     submitRace(5446921);
+//     submitRace(10858926);
+//     submitRace(16270931);
+//     submitRace(21682936);
+//     submitRace(209462);
+//     submitRace(18038548);
+//     return checkAnswer(21270);
+// }
 
 int main() {
 
-    uint32_t res = doEverything();
-    return static_cast<int>(res);
+    // uint32_t res = doEverything();
+    // return static_cast<int>(res);
     // checkAnswer(21270);
     // // HorseRiddle horseRiddle;
     // std::vector<uint32_t> myRace1 {0,1,2,3,4};
